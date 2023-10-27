ccf8ff63c6924fb5d80d74cc8b96259eff41d637
OkaniYoshiii <dragut31620@gmail.com>
2023-10-27 12:43:00 +0200

    Amélioration système de recherche de Mods
    - RegExp pour éviter les erreurs de frappe des Tags
    - Refactorisation du code pour changer les Valeurs désactivées ou non lorsque l'on change le tag sélectioné

--- Page-Mods/JS/main.js
+++ Page-Mods/JS/main.js
@@ -1,87 +1,41 @@
-const MODS = document.querySelectorAll('.mods > div');
-
-let tagsLists = new Array;
-let tags = new Array;
-
-MODS.forEach((MOD) => {
-    const TAGS_LIST = MOD.querySelector('.tags');
-    let tagsList = new Object;
-
-    const ELEMENT_TAGS = TAGS_LIST.textContent.split(',');
-    tagsList.tags = ELEMENT_TAGS;
-    tagsList.element = MOD;
-
-    tags.push(ELEMENT_TAGS);
-    tagsLists.push(tagsList);
-})
-
-tags = Array.from(new Set(tags.flat()))
-
+const MODS_TAGS = Array.from(document.querySelectorAll('.tags'));
+const REGEXP = /\s,\s|\s,|,\s/g; // targets : " ," / ", " and " , "
+const TAGS = [...new Set(MODS_TAGS.flatMap((MOD_TAGS) => {return MOD_TAGS.innerText.split(/\s,\s|\s,|,\s/g)}))];
 
 const DOCUMENT_FRAGMENT = new DocumentFragment();
-
-tags.forEach((tag) => {
+TAGS.forEach((TAG) => {
     const OPTION = document.createElement('option');
-    OPTION.textContent = tag;
-    OPTION.value = tag;
+    OPTION.textContent = TAG;
+    OPTION.value = TAG;
     DOCUMENT_FRAGMENT.appendChild(OPTION);
 })
 
-const SELECT_ELEMENTS = document.querySelectorAll('select');
+const SELECT_ELEMENTS = Array.from(document.querySelectorAll('select'));
 const SELECT_ELEMENTS_COUNT = SELECT_ELEMENTS.length;
 
-let selectedTags = new Array(SELECT_ELEMENTS_COUNT);
-let disabledOptions = new Object;
+document.querySelector('fieldset').addEventListener('change', updateModList);
 
+let selects = new Array(SELECT_ELEMENTS_COUNT);
 
-SELECT_ELEMENTS.forEach((select, index) => {
-    select.appendChild(DOCUMENT_FRAGMENT.cloneNode(true));
-    select.addEventListener('change',() => {
-        selectedTags[index] = select.value;
-        toggleOptions(select,index);
-        console.log("Option désactivée ", disabledOptions);
-        displayModsByTags();
-    })
-});
+function updateModList(ev) {
+    const SELECT = ev.target;
+    const SELECT_INDEX = SELECT_ELEMENTS.indexOf(SELECT);
 
+    const OLD_DISABLED_OPTION = selects[SELECT_INDEX];
 
-function toggleOptions(select, index) {
-    disabledOptions[index]?.forEach((OPTION) => {OPTION.toggleAttribute('disabled')});
-        
-    if(select.value) {
-        disabledOptions[index] = Array.from(document.querySelectorAll(`option[value="${select.value}"]`))
-        disabledOptions[index].forEach((OPTION) => {OPTION.toggleAttribute('disabled')});
-    } else {
-        disabledOptions[index] = null;
-    }
-}
-
-const OUTPUT = document.querySelector('.mod-count');
-const MODS_COUNT = MODS.length;
-
-OUTPUT.textContent = MODS_COUNT;
-
-function displayModsByTags() {
-    const HAS_SELECTED_TAGS = selectedTags.some((tag) => {return (tag)});
-    let selectedModsCount = 0;
-
-
-    for (let i = 0; i < tagsLists.length; i++) {
-        const ELEMENT = tagsLists[i].element;
-        if(!HAS_SELECTED_TAGS) {
-            selectedModsCount = MODS_COUNT;
-            ELEMENT.style.display = 'block';
-        } else {
-            const TAGS = tagsLists[i].tags;
-            const HAS_MATCHING_TAGS = TAGS.some((TAG) => {return selectedTags.includes(TAG)});
-            if(HAS_MATCHING_TAGS) {
-                ELEMENT.style.display = 'block';
-                selectedModsCount += 1;
-            } else {
-                ELEMENT.style.display = 'none';
-            }
-        }
+    if(OLD_DISABLED_OPTION) {
+        console.log(OLD_DISABLED_OPTION);
+        OLD_DISABLED_OPTION.forEach((OLD_OPTION) => OLD_OPTION.removeAttribute('disabled'));
     }
 
-    OUTPUT.textContent = selectedModsCount;
+    if(!SELECT.value) {selects[SELECT_INDEX] = ''; return;}
+
+    //Pour chaque SELECT, enregistrer quelle OPTION il a désactivé dans l'ensemble des SELECT
+    const DISABLED_OPTIONS = document.querySelectorAll(`option[value="${SELECT.value}"]`);
+    DISABLED_OPTIONS.forEach((OPTION) => OPTION.setAttribute('disabled',''));
+
+    selects[SELECT_INDEX] = DISABLED_OPTIONS;
 }
+
+SELECT_ELEMENTS.forEach((select, index) => {select.appendChild(DOCUMENT_FRAGMENT.cloneNode(true));});
+
--- default.js
+++ default.js
@@ -15,29 +15,4 @@ HAMBURGER_MENU.addEventListener('click', function() {
         HAMBURGER_MENU.setAttribute("data-expanded","true");
         return;
     }
-<<<<<<< Updated upstream
 })
-
-// Page transitions
-
-if (hasEnableAnimations === "false") document.querySelector(':root').classList.remove('js-active');
-
-const BODY = document.body;
-const ANCHOR_TAGS = document.querySelectorAll('a:not([target="_blank"])');
-
-ANCHOR_TAGS.forEach((ANCHOR_TAG) => {ANCHOR_TAG.addEventListener('click', transitionToNextPage)});
-
-function transitionToNextPage(ev) {
-    if (hasEnableAnimations === "false") return;
-    document.documentElement.classList.add('js-active');
-    const HREF = ev.currentTarget.href;
-    if(window.location.href == HREF) return;
-    ev.preventDefault();
-    BODY.classList.add('leave-page');
-    setTimeout(() => {
-        window.location.href = HREF;
-    }, 800);
-}
-=======
-})
->>>>>>> Stashed changes
