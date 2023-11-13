8fd92220a764538aa3e837176bbf9fe26af2832b
OkaniYoshiii <dragut31620@gmail.com>
2023-11-13 17:59:53 +0100

    Fichiers PHP + réorganisation des fichiers

---
+++ components/header.php
@@ -0,0 +1,21 @@
+<header>
+    <nav class="wide primary-navigation">
+        <a class="icon-link" href="index.php">
+            <img class="server-icon" src="../images/Compressed/FRT-Logo-75x75.webp" width="75" height="75" alt="">
+            <p class="website-title">LES FRATERNELS</p>
+        </a>
+        <div class="header-links">
+            <button class="hamburger" data-expanded="false">
+                <span class="burger-line"></span>
+                <span class="burger-line"></span>
+                <span class="burger-line"></span>
+            </button>
+            <ul aria-expanded="false">
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="mods.php">Mods</a></li>
+                <li><a href="liens-utiles.php">Liens Utiles</a></li>
+                <li><a href="documentation.php">Outils Modding</a></li>               
+            </ul>
+        </div>
+    </nav>
+</header>
---
+++ pages/fraternels-lab.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="OkaniYoshiii">
+    <meta name="description" content="[xxxxxxxxxxxxxxxxxx] du serveur 7 Days to Die &quot;Les Fraternels&quot;">
+    <script>document.documentElement.classList.add('js-active');</script>
+    <title>Laboratoire Fraternels</title>
+    <link rel="icon" type="image/vnd.microsoft.icon" href="../images/Compressed/FRT-Logo-30x30.ico" />
+    <link rel="stylesheet" href="../css/default.css">
+    <link rel="stylesheet" href="../css/fraternels-lab.css">
+    <script src="../js/default.js" defer></script>
+    <script src="../js/fraternels-lab.js" defer></script>
+</head>
+<body>
+    
+    <?php include_once "../components/header.php"; ?>
+
+    <main>
+        <section>
+            <div class="narrow">
+                <p>Extraction des <code>entitygroup</code> d'un fichier de Mod pour générer le code permettant de les supprimer (<code>remove xpath</code>) :</p>
+                <pre id="entitygroups-output"></pre>
+            </div>
+        </section>
+    </main>
+
+    <?php include_once "../components/footer.php"; ?>
+
+</body>
+</html>
